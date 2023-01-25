8cae290abda3d17e01bd14975cd9025b49bc0331
NTAThinh <122768309+NTAThinh@users.noreply.github.com>
2023-01-25 13:49:59 +0700

    Delete CoffeeManagement-main/CoffeeManagement-main directory

--- CoffeeManagement-main/CoffeeManagement-main/coffee.zip_expanded/coffee/src/main/java/project/coffee/dao/CoffeeDAO.java
+++
@@ -1,9 +0,0 @@
-package project.coffee.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import project.coffee.model.Coffee;
-
-public interface CoffeeDAO extends CrudRepository<Coffee, Integer>{
-
-}

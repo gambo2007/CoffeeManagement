feec235e1f52421a65071225389725450412e0be
NTAThinh <122768309+NTAThinh@users.noreply.github.com>
2023-01-25 13:43:03 +0700

    Delete coffee.zip_expanded directory

--- coffee.zip_expanded/coffee/src/main/java/project/coffee/dao/OrderCoffeeDAO.java
+++
@@ -1,9 +0,0 @@
-package project.coffee.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import project.coffee.model.OrderCoffee;
-
-public interface OrderCoffeeDAO extends CrudRepository<OrderCoffee, Integer>{
-
-}
